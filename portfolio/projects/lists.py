d8f1685a5a0c2602d482bd804f494e1cf0d589f8
mwong068 <mwong068@gmail.com>
2020-06-20 16:40:06 -0700

    added pop function to projects class

--- portfolio/projects/lists.py
+++ portfolio/projects/lists.py
@@ -1,9 +1,12 @@
 class Projects:
     def __init__(self):
-        self.storage = []
+        self._storage = []
 
     def __len__(self):
-        return len(self.storage)
+        return len(self._storage)
 
     # def push(self, item):
     #     self._storage.append(item)
+
+    def pop(self, item):
+        self._storage.pop(item)
