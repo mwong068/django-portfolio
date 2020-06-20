6bf0889532db5c32fac0bb5e8e3110f8676d714a
mwong068 <mwong068@gmail.com>
2020-06-19 21:35:06 -0700

    added testing

---
+++ portfolio/projects/lists.py
@@ -0,0 +1,9 @@
+class Projects:
+    def __init__(self):
+        self.storage = []
+
+    def __len__(self):
+        return len(self.storage)
+
+    # def push(self, item):
+    #     self._storage.append(item)
---
+++ portfolio/projects/tests/test_lists.py
@@ -0,0 +1,17 @@
+from projects.lists import Projects
+import pytest
+
+# @pytest.fixture
+# def project():
+#   return Projects
+
+def test_constructor():
+  a = Projects()
+  assert isinstance(a, Projects)
+  assert len(a) == 0
+
+# def test_push(project):
+#   project.push(3)
+#   assert len(project) == 1
+#   project.push(5)
+#   assert len(project) == 2
