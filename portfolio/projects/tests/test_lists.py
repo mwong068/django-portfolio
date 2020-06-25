939c4eb38d426222db5296b30134a4b9c2e6a6a3
mwong068 <mwong068@gmail.com>
2020-06-25 13:31:40 -0700

    finished tests for class

--- portfolio/projects/tests/test_lists.py
+++ portfolio/projects/tests/test_lists.py
@@ -1,20 +1,20 @@
 from projects.lists import Projects
 import pytest
 
-# @pytest.fixture
-# def project():
-#   return Projects
+@pytest.fixture
+def project():
+  return Projects
 
 def test_constructor():
   a = Projects()
   assert isinstance(a, Projects)
   assert len(a) == 0
 
-# def test_push(project):
-#   project.push(3)
-#   assert len(project) == 1
-#   project.push(5)
-#   assert len(project) == 2
+def test_push(project):
+  project.push(3)
+  assert len(project) == 1
+  project.push(5)
+  assert len(project) == 2
 
 def test_pop(project):
   stack.push('hello')
